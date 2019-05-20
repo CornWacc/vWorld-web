efb84f4fa22412fbee57437d19ce82e3c9b7caf6
Yyc <791621554@qq.com>
2019-05-20 16:22:34 +0800

    修改

--- vWorld-web/src/router/index.js
+++ vWorld-web/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/user/Login'
 import BackStage from "@/components/administrator/BackStage"
-import UserList from "@/components/userList/UserLIst"
+import UserList from "@/components/userList/UserList"
 import ShopMain from "@/components/main/ShopMain"
 import Reg from '@/components/user/Reg'
 import RoleList from "@/components/role/RoleList"
